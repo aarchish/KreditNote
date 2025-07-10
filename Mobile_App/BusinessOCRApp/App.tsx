3635030325282d807b025d6296cfe98e962a8607
Aarchish Jindal <aarchish0@gmail.com>
2025-07-10 14:22:45 +0530

    switched to RN-Expo

--- Mobile_App/BusinessOCRApp/App.tsx
+++ Mobile_App/BusinessOCRApp/App.tsx
@@ -1,21 +1,20 @@
-// App.tsx
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import CameraScreen from './src/screens/CameraScreen';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <CameraScreen />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
-
-export default App;
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
